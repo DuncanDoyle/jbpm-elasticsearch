777f3a1fd71d9a595eb1006b21ea3534b17eeb31
Duncan Doyle <Duncan.Doyle@gmail.com>
2016-09-13 23:22:37 -0400

    Implemented afterProcessCompleted test.

--- jbpm-elasticsearch-persistence/src/test/java/org/jbpm/elasticsearch/persistence/listener/ElasticSeachProcessEventListenerTest.java
+++ jbpm-elasticsearch-persistence/src/test/java/org/jbpm/elasticsearch/persistence/listener/ElasticSeachProcessEventListenerTest.java
@@ -12,6 +12,7 @@ import org.jbpm.elasticsearch.persistence.context.ProcessEventContext;
 import org.jbpm.elasticsearch.persistence.context.ProcessEventContext.ProcessState;
 import org.junit.Before;
 import org.junit.Test;
+import org.kie.api.event.process.ProcessCompletedEvent;
 import org.kie.api.event.process.ProcessEventListener;
 import org.kie.api.event.process.ProcessStartedEvent;
 import org.kie.api.runtime.Environment;
@@ -23,60 +24,95 @@ import org.mockito.Mockito;
 public class ElasticSeachProcessEventListenerTest {
 
 	private ProcessEventListener listener;
-	
+
 	@Before
 	public void before() {
 		String elasticSearchEndpointUrl = "http://localhost:9800";
 		listener = new ElasticSearchProcessEventListener(elasticSearchEndpointUrl);
 	}
-	
-	
+
 	/**
 	 * Test that the process-state on the context is set to "STARTING".
 	 * 
 	 */
 	@Test
 	public void testAfterProcessStarted() throws Exception {
-		//We need to mock this stuff.
+		// We need to mock this stuff.
 		ProcessStartedEvent event = Mockito.mock(ProcessStartedEvent.class);
 		ProcessInstance pInstance = Mockito.mock(ProcessInstance.class);
 		Mockito.when(event.getProcessInstance()).thenReturn(pInstance);
 		Mockito.when(pInstance.getProcessId()).thenReturn("testProcessId");
 		Mockito.when(pInstance.getState()).thenReturn(ProcessInstance.STATE_ACTIVE);
-		
+
 		KieRuntime kieRuntime = Mockito.mock(KieRuntime.class);
 		Environment environment = Mockito.mock(Environment.class);
 		Mockito.when(event.getKieRuntime()).thenReturn(kieRuntime);
 		Mockito.when(kieRuntime.getEnvironment()).thenReturn(environment);
 		Mockito.when(environment.get("deploymentId")).thenReturn("testDeploymentId");
-		
-		//We also need to mock the TM, as the "afterProcessCompleted" event will try to register a transactionsynchronization.
+
+		// We also need to mock the TM, as the "afterProcessCompleted" event will try to register a transactionsynchronization.
 		TransactionManager tmManager = Mockito.mock(TransactionManager.class);
 		Mockito.when(environment.get(EnvironmentName.TRANSACTION_MANAGER)).thenReturn(tmManager);
-		
+
 		listener.afterProcessStarted(event);
-		
-		//Retrieve the context.
+
+		// Retrieve the context.
 		Field threadLocalProcessContextField = listener.getClass().getDeclaredField("processContext");
 		threadLocalProcessContextField.setAccessible(true);
-		ThreadLocal<ProcessEventContext> threadLocalProcessContext = (ThreadLocal<ProcessEventContext>) threadLocalProcessContextField.get(listener); 
+		ThreadLocal<ProcessEventContext> threadLocalProcessContext = (ThreadLocal<ProcessEventContext>) threadLocalProcessContextField
+				.get(listener);
 		ProcessEventContext processContext = threadLocalProcessContext.get();
-		
-		//Assert that the correct IDs have been set on the context.
+
+		// Assert that the correct IDs have been set on the context.
 		assertEquals("testProcessId", processContext.getProcessId());
 		assertEquals("testDeploymentId", processContext.getDeploymentUnit());
-		
-		//Assert the process state.
+
+		// Assert the process state.
 		assertEquals("ACTIVE", processContext.getProcessState());
 		assertEquals(ProcessState.STARTING, processContext.getIndexingProcessState());
-		
-		//Assert that the TransactionSynchronization got registered with the TM.
+
+		// Assert that the TransactionSynchronization got registered with the TM.
 		verify(tmManager, times(1)).registerTransactionSynchronization(anyObject());
 	}
 
 	@Test
-	public void testAfterProcessCompleted() {
+	public void testAfterProcessCompleted() throws Exception {
+		// We need to mock this stuff.
+		ProcessCompletedEvent event = Mockito.mock(ProcessCompletedEvent.class);
+		ProcessInstance pInstance = Mockito.mock(ProcessInstance.class);
+		Mockito.when(event.getProcessInstance()).thenReturn(pInstance);
+		Mockito.when(pInstance.getProcessId()).thenReturn("testProcessId");
+		Mockito.when(pInstance.getState()).thenReturn(ProcessInstance.STATE_COMPLETED);
 
+		KieRuntime kieRuntime = Mockito.mock(KieRuntime.class);
+		Environment environment = Mockito.mock(Environment.class);
+		Mockito.when(event.getKieRuntime()).thenReturn(kieRuntime);
+		Mockito.when(kieRuntime.getEnvironment()).thenReturn(environment);
+		Mockito.when(environment.get("deploymentId")).thenReturn("testDeploymentId");
+
+		// We also need to mock the TM, as the "afterProcessCompleted" event will try to register a transactionsynchronization.
+		TransactionManager tmManager = Mockito.mock(TransactionManager.class);
+		Mockito.when(environment.get(EnvironmentName.TRANSACTION_MANAGER)).thenReturn(tmManager);
+
+		listener.afterProcessCompleted(event);
+
+		// Retrieve the context.
+		Field threadLocalProcessContextField = listener.getClass().getDeclaredField("processContext");
+		threadLocalProcessContextField.setAccessible(true);
+		ThreadLocal<ProcessEventContext> threadLocalProcessContext = (ThreadLocal<ProcessEventContext>) threadLocalProcessContextField
+				.get(listener);
+		ProcessEventContext processContext = threadLocalProcessContext.get();
+
+		// Assert that the correct IDs have been set on the context.
+		assertEquals("testProcessId", processContext.getProcessId());
+		assertEquals("testDeploymentId", processContext.getDeploymentUnit());
+
+		// Assert the process state.
+		assertEquals("COMPLETED", processContext.getProcessState());
+		assertEquals(ProcessState.COMPLETING, processContext.getIndexingProcessState());
+
+		// Assert that the TransactionSynchronization got registered with the TM.
+		verify(tmManager, times(1)).registerTransactionSynchronization(anyObject());
 	}
 
 	@Test
@@ -92,10 +128,10 @@ public class ElasticSeachProcessEventListenerTest {
 	public void testProcessContext() {
 
 	}
-	
+
 	@Test
 	public void testTransactionSynchronizationRegistration() {
-		
+
 	}
 
 }
